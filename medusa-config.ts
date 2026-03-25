6e106bb95d2dccc39ff03a4382c06926f34b0a4d
Christian Leon Olsen <christianleono23@gmail.com>
2026-03-25 13:15:23 +0100

    Update config to disable ssl

--- medusa-config.ts
+++ medusa-config.ts
@@ -12,6 +12,14 @@ module.exports = defineConfig({
       authCors: process.env.AUTH_CORS!,
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
-    }
+    },
+    
+    databaseDriverOptions: {
+      connection: {
+        ssl: {
+          rejectUnauthorized: false
+        }
+      }
+    },
   }
 })
